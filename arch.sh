66c9d1fb9f1caacd2b01fcf91f2cf6b710b4ed64
Akhil Narang <me@akhilnarang.dev>
2020-11-07 19:57:23 +0530

    scripts: add in a basic script to automate my zsh config
    - Also check in my powerlevel10k configuration
    
    Author:    Akhil Narang <me@akhilnarang.dev>
    Signed-off-by: P.M.SALMAN KHAN  <patanmohammadsalmankhan@gmail.com>

--- arch.sh
+++ arch.sh
@@ -10,4 +10,9 @@ bash setup/build-env.sh
 echo ' '
 echo 'Setup of Android Build Envirnoment Is Done."
 sleep 2
+echo ' '
+echo 'Setting Up Oh My ZSH Theme.'
+echo 'Making powerlevel10k default theme.'
+bash zsh/zsh.sh
+echo 'Want to Customize the look as per your liking ? Head Over to this article "https://bit.ly/3KQanlE".'
 echo 'Your Are Completely Read To Rock.'
