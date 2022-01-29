ab38c5241730eb346b1c70a261174edb8b516b97
SALMANKHANPM <patanmohammadsalmankhan@gmail.com>
2022-01-29 16:40:12 +0530

    scripts : add script to build glibc 2.34 .
    - useful to build a toolchain  which req glibc version 2.34
    - Thanks to Dakkshesh for repo.
    
    Signed-off-by: P.M.SALMAN KHAN  <patanmohammadsalmankhan@gmail.com>

--- arch.sh
+++ arch.sh
@@ -20,6 +20,10 @@ echo 'Setting Up SSH For easier usage of various tools.'
 bash setup/sshid.sh
 echo ' '
 sleep 2
+echo 'Initalizing Requirement for Glibc 2.34.'
+bash setup/glibc.sh
+sleep 2
+echo ' '
 echo 'Setting Up Oh My ZSH Theme.'
 echo 'Making powerlevel10k default theme.'
 bash zsh/zsh.sh
---
+++ setup/glibc.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+echo 'Building and installing glibc version 2.34.'
+packages="glibc-pkgbuild"
+for package in $packages; do
+    echo "Installing $package"
+    git clone https://github.com/SALMANKHANPM/"$package".git
+    cd "$package" || exit
+    makepkg -si --skippgpcheck --noconfirm --needed
+    cd - || exit
+    rm -rf "$package"
+done
+
+# A Huge Thanks to Dakkshesh For This ...
