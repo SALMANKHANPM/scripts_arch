d67ee2930df80e5104735d49877a7ab7e4a68372
P.M.SALMAN KHAN <67056107+SALMANKHANPM@users.noreply.github.com>
2022-02-20 12:47:12 +0530

    glibc : track from the main repo

--- setup/glibc.sh
+++ setup/glibc.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 echo 'Building and installing glibc version 2.34.'
-packages="glibc-pkgbuild"
+packages="dakkshesh07/glibc-pkgbuild"
 for package in $packages; do
     echo "Installing $package"
-    git clone https://github.com/SALMANKHANPM/"$package".git
+    git clone https://github.com/"$package".git
     cd "$package" || exit
     makepkg -si --skippgpcheck
     cd - || exit
