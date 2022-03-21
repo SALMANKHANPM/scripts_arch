ec9a4fb2b730e105f7c5c6bbf68fdfddb70f0101
P.M.SALMAN KHAN <67056107+SALMANKHANPM@users.noreply.github.com>
2022-03-21 19:13:16 +0530

    glibc : update url !

--- setup/glibc.sh
+++ setup/glibc.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 echo 'Building and installing glibc version 2.34.'
-packages="dakkshesh07/glibc-pkgbuild"
+packages= "glibc-pkgbuild"
 for package in $packages; do
     echo "Installing $package"
-    git clone https://github.com/"$package".git
+    git clone https://github.com/dakkshesh07/"$package".git
     cd "$package" || exit
     makepkg -si --skippgpcheck
     cd - || exit
